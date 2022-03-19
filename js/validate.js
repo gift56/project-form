25c19a599d2ab38aab6f0c86340d84fd4abc04c2
gift56 <efeasiughu@gmail.com>
2022-03-19 13:17:23 +0100

    added validation

---
+++ js/validate.js
@@ -0,0 +1,22 @@
+const submits = document.querySelector("#my_form");
+const nameInput = document.querySelector("#name");
+const lastInput = document.querySelector("#last");
+const emailInput = document.querySelector("#email");
+const errors = document.querySelector("#msg");
+
+submits.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    if (nameInput.value === "" || lastInput.value === "" || emailInput === "") {
+        errors.classList.add("err");
+        errors.innerHTML="Please enter all fields";
+
+        setTimeout(() => errors.remove(), 3000);
+
+    } else {
+        console.log("Success");
+    }
+}
+
