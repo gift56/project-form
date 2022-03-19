65d33f83bfe356b876e068bfa255123d41293dbb
gift56 <efeasiughu@gmail.com>
2022-03-19 13:24:57 +0100

    vallidation js

--- js/validate.js
+++ js/validate.js
@@ -11,12 +11,16 @@ function onSubmit(e) {
 
     if (nameInput.value === "" || lastInput.value === "" || emailInput === "") {
         errors.classList.add("err");
-        errors.innerHTML="Please enter all fields";
+        errors.innerHTML = "Please enter all fields";
 
         setTimeout(() => errors.remove(), 3000);
 
     } else {
+        errors.classList.add("suc");
+        errors.innerHTML = "Success";
         console.log("Success");
+
+        setTimeout(() => errors.remove(), 2000);
     }
 }
 
